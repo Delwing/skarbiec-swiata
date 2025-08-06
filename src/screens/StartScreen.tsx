44999e730d9b73e3b77888290f2102a811e15192
Piotr Wilczyński <delwing@gmail.com>
2025-08-06 14:06:21 +0200

    add start screen

--- src/screens/StartScreen.tsx
+++ src/screens/StartScreen.tsx
@@ -2,11 +2,14 @@ import './StartScreen.css'
 import logo from '../assets/logo.svg'
 
 function StartScreen() {
-  return (
-    <div className="start-screen">
-      <img src={logo} alt="Logo" />
-    </div>
-  )
+    return (
+        <div className="start-screen">
+            <img src={logo} alt="Logo"/>
+            <div role="button" className={'start-button'}>
+                Start
+            </div>
+        </div>
+    )
 }
 
 export default StartScreen
