780aab782006b1d351650b89077ab709d27c9a49
Piotr Wilczyński <delwing@gmail.com>
2025-08-06 14:44:41 +0200

    add sample definition

--- src/components/definitions.ts
+++ src/components/definitions.ts
@@ -1,3 +1,5 @@
+import Bear from "../assets/items/bear.svg"
+
 export interface ItemDefinition {
     image: string
     x: number
@@ -9,7 +11,13 @@ export interface ItemDefinition {
 
 
 
-
 export const itemDefinitions: ItemDefinition[] = [
-
+    {
+        image: Bear,
+        x: 0,
+        y: 0,
+        descriptionImage: "",
+        descriptionText: "",
+        descriptionTitle: ""
+    }
 ]
