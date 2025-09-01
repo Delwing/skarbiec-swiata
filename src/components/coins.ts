7ad130f61448836f98fa61a5fb6b20c25ef4efb6
Piotr Wilczynski <delwing@gmail.com>
2025-09-01 02:05:27 +0200

    fix coin ids

--- src/components/coins.ts
+++ src/components/coins.ts
@@ -10,7 +10,7 @@ export const coins: CoinDefinition[] = [
         id: "1",
         x: 1058,
         y: 560,
-        descriptionText: `Czy wiesz, że porcelanowe muszle kauri były najdłużej obowiązującą globalną walutą (tak zwanym płacidłem) od czasów starożytnych do niemal współczesnych, od Azji, przez Bliski i Daleki Wschód, Afrykę po Amerykę Północną? Płacono nimi zanim pojawiły się pieniądze z kruszcu (srebra, złota, innych metali). Były trwałe, cenne, policzalne, trudno dostępne (trzymały wartość) i symbolizowały bogactwo. Pierwotnie najbardziej rozpowszechnione były w okolicach Malediwów (Azja), gdzie żyło najwięcej tych mięczaków.`,
+        descriptionText: `Czy wiesz, że porcelanowe muszle kauri były najdłużej obowiązująca globalną walutą (tak zwanym płacidłem) od czasów starożytnych do niemal współczesnych, od Azji, przez Bliski i Daleki Wschód, Afrykę po Amerykę Północną? Płacono nimi zanim pojawiły się pieniądze z kruszcu (srebra, złota, innych metali). Były trwałe, cenne, policzalne, trudno dostępne (trzymały wartość) i symbolizowały bogactwo. . Pierwotnie najbardziej rozpowszechnione były w okolicach Malediwów (Azja), gdzie żyło najwięcej tych mięczaków.`,
     },
     {
         id: "2",
@@ -26,9 +26,9 @@ export const coins: CoinDefinition[] = [
     },
     {
         id: "4",
-        x: 781,
-        y: 500,
-        descriptionText: `Manile – otwarte bransolety z brązu, miedzi lub mosiądzu – stały się ważnym środkiem płatniczym w handlu z Europejczykami w Afryce od XV wieku, niestety odgrywając główną rolę w handlu niewolnikami.`,
+        x: 834,
+        y: 398,
+        descriptionText: `Starożytni Grecy jako pierwsi umieszczali na monetach wizerunki bóstw i władców, czyniąc z nich narzędzie propagandy. Podstawową monetą używaną w starożytnej Grecji była drachma, dzielona na 6 oboli lub 48 chalków. Rzymianie z kolei stworzyli jednolity system monetarny oparty na denarze, który ułatwiał handel w całym imperium i przetrwał setki lat.`,
     },
     {
         id: "5",
@@ -46,25 +46,25 @@ export const coins: CoinDefinition[] = [
         id: "7",
         x: 751,
         y: 403,
-        descriptionText: `Nazwa najpopularniejszej waluty świata, dolara, pochodzi od srebrnej monety bitej w XVI wieku w Jáchymovie przez czeskiego hrabiego Schlicka. Monetę tę nazywano "Joachimsthaler", co skrócono do "talar", a z czasem przekształciło się w "dolar".`,
-    },
-    {
-        id: "8",
-        x: 328,
-        y: 388,
         descriptionText: `Dolar amerykański wywodzi się bezpośrednio od dolara hiszpańskiego. Hiszpania produkowała swoje monety w koloniach Ameryki z powodu braku kruszcu w Europie, co uczyniło dolar hiszpański popularnym i doprowadziło do przyjęcia dolara jako waluty Stanów Zjednoczonych w 1785 roku.`,
     },
     {
-        id: "9",
+        id: "8",
         x: 377,
         y: 257,
         descriptionText: `Wampum – pasy i sznury koralików wykonanych z fioletowych i białych muszli – pełniły rolę waluty u rdzennych mieszkańców Ameryki, służąc także do zawierania traktatów, przekazywania informacji i celów ceremonialnych.`,
     },
     {
+        id: "9",
+        x: 463,
+        y: 403,
+        descriptionText: `Banknot 1000 franków szwajcarskich jest najcenniejszym banknotem na świecie, który jest używany na co dzień. Warty jest około 1150 dolarów amerykańskich i bardzo trudno go podrobić.`,
+    },
+    {
         id: "10",
-        x: 834,
-        y: 398,
-        descriptionText: `Starożytni Grecy jako pierwsi umieszczali na monetach wizerunki bóstw i władców, czyniąc z nich narzędzie propagandy. Podstawową monetą używaną w starożytnej Grecji była drachma, dzielona na 6 oboli lub 48 chalków. Rzymianie z kolei stworzyli jednolity system monetarny oparty na denarze, który ułatwiał handel w całym imperium i przetrwał setki lat.`,
+        x: 328,
+        y: 388,
+        descriptionText: `Nazwa najpopularniejszej waluty świata, dolara, pochodzi od srebrnej monety bitej w XVI wieku w Jáchymovie przez czeskiego hrabiego Schlicka. Monetę tę nazywano "Joachimsthaler", co skrócono do "talar", a z czasem przekształciło się w "dolar".`,
     },
     {
         id: "11",
@@ -80,45 +80,44 @@ export const coins: CoinDefinition[] = [
     },
     {
         id: "13",
-        x: 463,
-        y: 403,
-        descriptionText: `Banknot 1000 franków szwajcarskich jest najcenniejszym banknotem na świecie, który jest używany na co dzień. Warty jest około 1150 dolarów amerykańskich i bardzo trudno go podrobić.`,
-    },
-    {
-        id: "14",
         x: 858,
         y: 573,
         descriptionText: `W wielu społecznościach rolniczych, na przykład wśród Masajów, to bydło było głównym miernikiem bogactwa i środkiem płatniczym przy ważnych transakcjach, jak zawarcie małżeństwa. Egipcjanie do dziś proponują zapłatę za żonę w wielbłądach. Dawno temu w Afryce jako pieniądz służyły również sól, muszelki, koraliki, metale, biżuteria, tkaniny, broń i narzędzia – liczyło się to, co było akceptowane i wartościowe dla ludzi.`,
     },
     {
+        id: "14",
+        x: 781,
+        y: 500,
+        descriptionText: `Manile – otwarte bransolety z brązu, miedzi lub mosiądzu – stały się ważnym środkiem płatniczym w handlu z Europejczykami w Afryce od XV wieku, niestety odgrywając główną rolę w handlu niewolnikami.`,
+    },
+    {
         id: "15",
+        x: 909,
+        y: 661,
+        descriptionText: `W 2008 roku hiperinflacja w Zimbabwe doprowadziła do emisji banknotu o nominale 100 bilionów dolarów zimbabweńskich, który szybko stał się niemal bezwartościowy. Wkrótce kraj zrezygnował z własnej waluty na rzecz obcych.`,
+    },
+    {
+        id: "16",
         x: 1175,
         y: 315,
         descriptionText: `Tugrik, znany również jako tögrög, jest oficjalną walutą Mongolii, a jego nazwa pochodzi od mongolskiego słowa oznaczającego „okrągły”, odnoszącego się do kształtu monet.`,
     },
     {
-        id: "16",
+        id: "17",
         x: 1241,
         y: 614,
         descriptionText: `Najbardziej wartościowym banknotem na świecie był banknot 10 000 dolarów singapurskich, wart około 30 000 złotych. Używano go w obiegu do 2014 roku i nadal można nim legalnie płacić.`,
     },
     {
-        id: "17",
+        id: "18",
         x: 718,
         y: 309,
         descriptionText: `Pierwszy na świecie bankomat został zainstalowany w Londynie w 1967 roku przez Barclays Bank. Zamiast kart używano specjalnych voucherów z jednorazowym kodem, a konstruktor wzorował się na maszynie wydającej gumy do żucia.`,
     },
     {
-        id: "18",
+        id: "19",
         x: 846,
         y: 930,
         descriptionText: `Na Antarktydzie istnieje dolar antarktyczny, lecz nie jest to oficjalny środek płatniczy. Na kontynencie płaci się dolarami amerykańskimi, euro oraz walutami obowiązującymi na statkach kursujących do Antarktydy.`,
     },
-    {
-        id: "19",
-        x: 909,
-        y: 661,
-        descriptionText: `W 2008 roku hiperinflacja w Zimbabwe doprowadziła do emisji banknotu o nominale 100 bilionów dolarów zimbabweńskich, który szybko stał się niemal bezwartościowy. Wkrótce kraj zrezygnował z własnej waluty na rzecz obcych.`,
-    },
 ]
-
